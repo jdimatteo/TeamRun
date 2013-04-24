638b52a4172ec3a1250f2b8288171b937e0a15fd
John DiMatteo <jdimatteo@gmail.com>
2013-04-24 08:34:01 -0400

    Current and average pace labels are now populated

--- TeamRun/TeamRun/TeamRunViewController.m
+++ TeamRun/TeamRun/TeamRunViewController.m
@@ -23,6 +23,8 @@
 @property (weak, nonatomic) IBOutlet UITextView *scrollingText;
 @property (weak, nonatomic) IBOutlet UILabel *timeRanLabel;
 @property (weak, nonatomic) IBOutlet UIButton *startStopButton;
+@property (weak, nonatomic) IBOutlet UILabel *currentPaceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *averagePaceLabel;
 
 - (void)createMatch;
 - (void)playerAuthenticated;
@@ -207,7 +209,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//   GPS Location Manager Related Methods
+//   GPS PSLocationManager Related Methods
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -237,14 +239,63 @@
     const double metersPerSecond = [PSLocationManager sharedLocationManager].currentSpeed;
     [self log:@"current speed: %f m/s", metersPerSecond];
     
-    //[self.currentPaceLabel setText:minutesPerMilePaceString(metersPerSecond)];
+    [self.currentPaceLabel setText:minutesPerMilePaceString(metersPerSecond)];
     
-    //[self.averagePaceLabel setText:minutesPerMilePaceString(distance/[self.model currentRunDuration:kCFGregorianUnitsSeconds].seconds)];
+    [self.averagePaceLabel setText:minutesPerMilePaceString(distance/[PSLocationManager sharedLocationManager].totalSeconds)];
 }
 
 - (void)locationManager:(PSLocationManager *)locationManager error:(NSError *)error {
     // location services is probably not enabled for the app
     [self log:@"Unable to determine location"];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   Utility Methods
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+NSString* minutesPerMilePaceString(const double metersPerSecond)
+{
+    /* e.g. if currentSpeed = 1.95 (meters per second),
+     *      then that is ~ 0.00121167345 miles per second (1.95 * MILES_PER_METER = 0.00121167),
+     *      which is ~ 0.072700407 miles per minute (0.00121167345 * 60 = 0.072700407),
+     *      which is ~ 13.7550811786 minutes per mile (1/0.072700407 = 13.7550811786),
+     *      which is ~ 13:45 pace (minutes:second) per mile (.7550811786*60 = 45.304870716)
+     *
+     * it is easier to first get secondsPerMile pace, round that, and then
+     * convert the rounded number to minutes per mile.
+     *
+     * e.g. 1.95 * MILES_PER_METER = 0.00121167 miles per second,
+     *      1/0.00121167 = 825.307220613 seconds per mile,
+     *      rounds to 825 seconds,
+     *      floor(825/60) = 13 minutes,
+     *      825 % 60 = 45 seconds,
+     *      so the pace is 13:45 (minutes:seconds per mile)
+     */
+    
+    static const double MILES_PER_METER = 0.000621371;
+
+    const double milesPerSecond = metersPerSecond * MILES_PER_METER;
+    
+    if (milesPerSecond > 0) // prevent divide by zero errors
+    {
+        const int secondsPerMile = round(1.0 / milesPerSecond);
+        
+        const int paceMinutes = secondsPerMile / 60;
+        const int paceSeconds = secondsPerMile % 60;
+        
+        if (paceMinutes > 59)
+        {
+            return @"0:00";
+        }
+        else
+        {
+            return [NSString stringWithFormat:@"%d:%02d", paceMinutes, paceSeconds];
+        }
+    }
+    return @"0:00";
 }
 
 
