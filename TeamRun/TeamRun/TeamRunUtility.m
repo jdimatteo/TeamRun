fc1bcc6d3dad1d9fba06cc2fa8db4c1567bcb2fa
John DiMatteo <jdimatteo@gmail.com>
2013-05-24 04:21:57 -0400

    specified gpl v3 licence below my Copyright -- only PSLocationManger isn't my Copyright and so isn't covered by the GPL

--- TeamRun/TeamRun/TeamRunUtility.m
+++ TeamRun/TeamRun/TeamRunUtility.m
@@ -5,6 +5,20 @@
 //  Created by John DiMatteo on 4/26/13.
 //  Copyright (c) 2013 John DiMatteo. All rights reserved.
 //
+//
+// This file is part of TeamRun.
+//
+// TeamRun is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// TeamRun is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TeamRun.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "TeamRunUtility.h"
 
